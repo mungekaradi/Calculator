aee4c326792f789031f4b03f4ba4dd71921a49c1
Aditya Mungekar <124152883+mungekaradi@users.noreply.github.com>
2025-01-26 18:41:07 +0530

    Update cal.js
    added comments

--- cal.js
+++ cal.js
@@ -1,13 +1,16 @@
+// display the value in input box.
 function pick(val)
 {
     document.getElementById('Cal').value += val;
 }
+// solves the equation.
 function solve()
 {
    let x = document.getElementById('Cal').value;
    let y = eval(x);
    document.getElementById('Cal').value = y;
 }
+// clears the input box.
 function clr()
 {
     document.getElementById('Cal').value=" ";
